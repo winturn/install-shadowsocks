c08acd476731774e05b787c494ec7d3a7b2d6ff8
winturn <win_turn@163.com>
2018-06-19 16:35:00 +0800

    Update install_shadowsocks.sh
    把ifconfig命令改为ip

--- install_shadowsocks.sh
+++ install_shadowsocks.sh
@@ -4,7 +4,7 @@
 # Operating System  : Centos 6+ x86_64
 #                     Centos 7+ x86_64
 # Author            : win turn
-# Update time       ：2017-11-27 21:09:15
+# Update time       ：2018-6-19 16:33:09
 # ==============================================================
 
 cd
@@ -14,28 +14,27 @@ yum -y install openssl-devel
 yum -y install wget
 
 wget http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
-tar -jxvf Python-2.7.3.tar.bz2
-cd Python-2.7.3
+tar -jxvf Python-2.7.3.tar.bz2 
+cd Python-2.7.3  
 ./configure
 make all
-make install
+make install 
 mv /usr/bin/python /usr/bin/python2.6.6
-ln -s /usr/local/bin/python2.7 /usr/bin/python
+ln -s /usr/local/bin/python2.7 /usr/bin/python 
 sed -i '1s/python/python2.6.6/' /usr/bin/yum
-if [[ 'CentOS Linux release 7.2.1511 (Core) ' == *' 7.'* ]]
-then
-    sed -i '1s/python/python2.6.6/' /usr/libexec/urlgrabber-ext-down
-fi
 
 cd
-yum -y install python-setuptools
+yum install python-setuptools
 wget https://bootstrap.pypa.io/ez_setup.py -O - | python
+wget http://pypi.python.org/packages/source/d/distribute/distribute-0.6.10.tar.gz
+tar zxvf distribute-0.6.10.tar.gz
+cd distribute-0.6.10
+python setup.py install
+cd
 easy_install pip
-pip install distribute
-
 pip install shadowsocks
 
-IP_ADDR=`ip addr | tail -1 | awk '{print $4}'`
+IP_ADDR=`ip -a address | tail -1 | awk '{print $4}'`
 
 cat > /etc/shadowsocks.json << EOF
 {
@@ -58,27 +57,23 @@ EOF
 ssserver -c /etc/shadowsocks.json -d start
 if [ $? -eq 0 ]
 then
-    echo "shadowsocks running..."
+    echo "shadowsocks已启动..."
 else
-    echo "shadowsocks fail..."
+    echo "shadowsocks启动异常..."
 fi
 
 echo "
-    ==========================[ shadowsocks command ]=================================
-    |  start   shadowsocks server: ssserver -c /etc/shadowsocks.json -d start        |
-    |  stop    shadowsocks server: ssserver -c /etc/shadowsocks.json -d stop         |
-    |  restart shadowsocks server: ssserver -c /etc/shadowsocks.json -d restart      |
-    ==================================================================================
+    =========================[ shadowsocks command ]==========================
+    |  启动shadowsocks服务: ssserver -c /etc/shadowsocks.json -d start        |
+    |  关闭shadowsocks服务: ssserver -c /etc/shadowsocks.json -d stop         |
+    |  重启shadowsocks服务: ssserver -c /etc/shadowsocks.json -d restart      |
+    ==========================================================================
 
-    =========================[    win_turn remind   ]=================================
-    |                                                                                |
-    |  ip address:   $IP_ADDR                                                        |
-    |                                                                                |
-    |  port 1    :   8888                                                            |
-    |  password 1:   shadowsocks                                                     |
-    |                                                                                |
-    |  port 2    :   9999                                                            |
-    |  password 2:   shadowsocks                                                     |
-    |                                                                                |
-    ==================================================================================
+    =========================[    win_turn 提醒您   ]==========================
+    |  ip地址:   $IP_ADDR                                                     |
+    |  端 口1:   8888                                                         |
+    |  密 码1:   shadowsocks                                                  |
+    |  端 口2:   9999                                                         |
+    |  密 码2:   shadowsocks                                                  |
+    ==========================================================================
 "
