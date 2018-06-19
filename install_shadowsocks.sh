663c0e99fc9154d841968601aa9aa99f59734bac
winturn <win_turn@163.com>
2018-06-19 16:38:57 +0800

    Update install_shadowsocks.sh
    echo的中文改为英文

--- install_shadowsocks.sh
+++ install_shadowsocks.sh
@@ -57,23 +57,23 @@ EOF
 ssserver -c /etc/shadowsocks.json -d start
 if [ $? -eq 0 ]
 then
-    echo "shadowsocks已启动..."
+    echo "shadowsocks run success..."
 else
-    echo "shadowsocks启动异常..."
+    echo "shadowsocks run fail..."
 fi
 
 echo "
-    =========================[ shadowsocks command ]==========================
-    |  启动shadowsocks服务: ssserver -c /etc/shadowsocks.json -d start        |
-    |  关闭shadowsocks服务: ssserver -c /etc/shadowsocks.json -d stop         |
-    |  重启shadowsocks服务: ssserver -c /etc/shadowsocks.json -d restart      |
-    ==========================================================================
+    =========================[ shadowsocks command ]======================================
+    |  start shadowsocks server    : ssserver -c /etc/shadowsocks.json -d start          |
+    |  close shadowsocks server    : ssserver -c /etc/shadowsocks.json -d stop           |
+    |  restart shadowsocks server  : ssserver -c /etc/shadowsocks.json -d restart        |
+    ======================================================================================
 
-    =========================[    win_turn 提醒您   ]==========================
-    |  ip地址:   $IP_ADDR                                                     |
-    |  端 口1:   8888                                                         |
-    |  密 码1:   shadowsocks                                                  |
-    |  端 口2:   9999                                                         |
-    |  密 码2:   shadowsocks                                                  |
-    ==========================================================================
+    =========================[    win_turn remind you   ]=================================
+    |  ip地址:   $IP_ADDR
+    |  端 口1:   8888
+    |  密 码1:   shadowsocks
+    |  端 口2:   9999
+    |  密 码2:   shadowsocks
+    ======================================================================================
 "
